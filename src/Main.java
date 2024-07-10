de2bab3200b60a6704a3b27a27a75cc27edce01b
Igor Melo <igor0903@live.com>
2024-07-10 18:26:38 +0100

    melhorias

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import br.com.dio.desafio.dominio.Curso;
 import br.com.dio.desafio.dominio.Mentoria;
 
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
         Curso cUrso = new Curso();
@@ -9,6 +11,10 @@ public class Main {
         cUrso.setTitulo("Curso Java");
         cUrso.setDescricao("Aprender java com dio");
         cUrso.setCargaHoraria(30);
+
+        mentoria.setTitulo("Mentoria Python");
+        mentoria.setDescricao("Aprender java com dio Python");
+        mentoria.setData(LocalDate.now());
 
         System.out.println(cUrso);
         System.out.println(mentoria);
