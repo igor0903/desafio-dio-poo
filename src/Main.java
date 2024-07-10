ff57d65643b7d1bf19a38f613b8ac39d35c0b63f
Igor Melo <igor0903@live.com>
2024-07-10 18:22:43 +0100

    classe curso e mentoria

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso cUrso = new Curso();
+        Mentoria mentoria = new Mentoria();
+
+        cUrso.setTitulo("Curso Java");
+        cUrso.setDescricao("Aprender java com dio");
+        cUrso.setCargaHoraria(30);
+
+        System.out.println(cUrso);
+        System.out.println(mentoria);
     }
 }
